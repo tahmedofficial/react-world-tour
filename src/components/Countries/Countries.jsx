ed92058d34eb1df50cdfdbd92e9b9b5499bdccbb
tahmedofficial <tanvirofcuo@gmail.com>
2024-03-12 09:37:00 +0600

    showing visited country list

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -6,6 +6,7 @@ import './Countries.css'
 const Countries = () => {
 
     const [countries, setCountries] = useState([]);
+    const [visitedCountries, setVisitedCountries] = useState([])
 
     useEffect(() => {
         const url = 'https://restcountries.com/v3.1/all'
@@ -15,12 +16,29 @@ const Countries = () => {
 
     }, [])
 
+    const handleVisitedCountries = (country) => {
+        console.log(country);
+        const newVisitedCountries = [...visitedCountries, country]
+        setVisitedCountries(newVisitedCountries);
+    }
+
     return (
         <div>
             <h3>Countries: {countries.length}</h3>
+            <div>
+                <h5>Visited countries: {visitedCountries.length}</h5>
+                <ul>
+                    {
+                        visitedCountries.map(country => <li key={country.cca3}>{country.name.common}</li>)
+                    }
+                </ul>
+            </div>
             <div className="country_container">
                 {
-                    countries.map(country => <Country key={country.cca3} country={country}></Country>)
+                    countries.map(country => <Country
+                        key={country.cca3}
+                        handleVisitedCountries={handleVisitedCountries}
+                        country={country}></Country>)
                 }
             </div>
         </div>
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './Country.css'
-const Country = ({ country }) => {
+const Country = ({ country, handleVisitedCountries }) => {
 
     const { name, flags, population, capital, area, cca3 } = country;
 
@@ -10,16 +10,22 @@ const Country = ({ country }) => {
         setVisited(!visited);
     }
 
+    const passWithParams = () => handleVisitedCountries(country)
+
+
     return (
-        <div className='country'>
+        <div className={`country ${visited && "visietd"}`}>
             <img src={flags.png} alt="" />
-            <h3>Name: {name.common}</h3>
+            <h3 style={{ color: visited ? "purple" : "blue" }}>Name: {name.common}</h3>
             <p>Capital: {capital}</p>
             <p>Area: {area}</p>
             <p>Population: {population}</p>
             <p><small>Code: {cca3}</small></p>
+            <button onClick={passWithParams}>Mark visited</button>
+            <br />
+            <br />
             <button onClick={handleVisited}>{visited ? 'Visited' : 'Going'}</button>
-            {visited ? 'I have visited this country' : 'I want to visite'}
+            <span style={{ marginLeft: "10px" }}>{visited ? 'I have visited this country' : 'I want to visite'}</span>
         </div>
     );
 };
